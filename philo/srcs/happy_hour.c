97d69db2c987d5bf60264e420d3d78a579fa9e7e
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-16 16:56:10 +0200

    perfect

--- philo/srcs/happy_hour.c
+++ philo/srcs/happy_hour.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 12:12:27 by faventur          #+#    #+#             */
-/*   Updated: 2022/07/16 16:33:21 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/16 16:55:52 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	solitude(t_sophist *philo)
 {
-	t_man	*rules;
+	struct timeval	now;
+	t_man			*rules;
 
 	rules = philo->rules;
 	take_notes(philo, "has taken a fork");
-	countdown(philo, rules->time_to_die);
+	gettimeofday(&now, NULL);
+	printf("%lld %d %s\n", time_diff(&rules->start, &now)
+		+ philo->rules->time_to_die, philo->id + 1, "died");
 }
 
 void	happy_hour(t_sophist *philo, t_man *rules)
